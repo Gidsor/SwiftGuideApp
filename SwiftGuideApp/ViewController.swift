079798f5aae3e4842b26d2a05326d64992adab13
Vadim Denisov <gidsor@gmail.com>
2018-10-01 13:09:45 +0500

    Initial Commit

---
+++ SwiftGuideApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftGuideApp
+//
+//  Created by Vadim Denisov on 01/10/2018.
+//  Copyright © 2018 Vadim Denisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

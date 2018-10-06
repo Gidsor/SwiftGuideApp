a4791c850a1d52e6356fed25759ac44ce46a09ba
Vadim Denisov <gidsor@gmail.com>
2018-10-06 22:45:41 +0500

    Added DesignPatterns markdown

--- SwiftGuideApp/Model/Categories.swift
+++ SwiftGuideApp/Model/Categories.swift
@@ -38,6 +38,7 @@ class Categories {
     private func createPatternTutorials() -> [Tutorial] {
         var tutorials: [Tutorial] = []
         
+        tutorials.append(Tutorial(headerText: "Паттерны", name: "Паттерны проектирования", pathMark: "DesignPatterns", image: "design-patterns-icon"))
         tutorials.append(Tutorial(headerText: "Паттерны", name: "Одиночка", pathMark: "Playground", image: "xcode-icon"))
         tutorials.append(Tutorial(headerText: "Паттерны", name: "Прототип", pathMark: "Playground", image: "xcode-icon"))
         tutorials.append(Tutorial(headerText: "Паттерны", name: "Адаптер", pathMark: "Playground", image: "xcode-icon"))

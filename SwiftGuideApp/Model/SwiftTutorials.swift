5fa7a126ae5bfb7f19faeaa4d57a384023ad7608
Vadim Denisov <gidsor@gmail.com>
2018-10-13 01:21:29 +0500

    Added Error Handling markdown

--- SwiftGuideApp/Model/SwiftTutorials.swift
+++ SwiftGuideApp/Model/SwiftTutorials.swift
@@ -34,6 +34,7 @@ class SwiftTutorials {
         tutorials.append(Tutorial(headerText: header, name: "Объекты и классы", pathMark: "ObjectsAndClasses", image: "swift-icon"))
         tutorials.append(Tutorial(headerText: header, name: "Перечисления и структуры", pathMark: "EnumerationsAndStructures", image: "swift-icon"))
         tutorials.append(Tutorial(headerText: header, name: "Протоколы и расширения", pathMark: "ProtocolsAndExtensions", image: "swift-icon"))
+        tutorials.append(Tutorial(headerText: header, name: "Обработка ошибок", pathMark: "ErrorHandling", image: "swift-icon"))
         
         return tutorials
     }

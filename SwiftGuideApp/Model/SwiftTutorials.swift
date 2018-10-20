59dc8e53a1decd8b8080cfad8d32092dc9c5f6b9
Vadim Denisov <gidsor@gmail.com>
2018-10-20 23:49:25 +0500

    Added "String Interpolation" markdown

--- SwiftGuideApp/Model/SwiftTutorials.swift
+++ SwiftGuideApp/Model/SwiftTutorials.swift
@@ -91,6 +91,7 @@ class SwiftTutorials {
         tutorials.append(Tutorial(headerText: header, name: "Строка является типом значения", pathMark: "StringsAreValueTypes", image: "swift-icon"))
         tutorials.append(Tutorial(headerText: header, name: "Работа с символами", pathMark: "WorkingWithCharacters", image: "swift-icon"))
         tutorials.append(Tutorial(headerText: header, name: "Конкатенация строк и символов", pathMark: "ConcatenatingStringsAndCharacters", image: "swift-icon"))
+        tutorials.append(Tutorial(headerText: header, name: "Интерполяция строк", pathMark: "StringInterpolation", image: "swift-icon"))
         
         
         return tutorials
